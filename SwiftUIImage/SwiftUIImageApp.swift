e5af6f8812fd8efa3f88488238a1d49038cf552f
jyang <jTang@walkgame.com>
2021-10-13 17:18:25 +0800

    Initial Commit

---
+++ SwiftUIImage/SwiftUIImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIImageApp.swift
+//  SwiftUIImage
+//
+//  Created by Apple on 2021/10/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
