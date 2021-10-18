adac7ad7e54f1b394c04bb545b65737c66461884
jyang <jTang@walkgame.com>
2021-10-18 17:12:07 +0800

    重疊來達到遮罩效果,這邊帶入長方形視圖,並加入透明度

--- SwiftUIImage/ContentView.swift
+++ SwiftUIImage/ContentView.swift
@@ -13,16 +13,9 @@ struct ContentView: View {
             .resizable()
             .aspectRatio(contentMode: .fit)
             .overlay(
-                Text("1. Every Cloudy Has A Sliver Line.\n2. Every Cloudy Has A Sliver Line.\n3. Every Cloudy Has A Sliver Line.\n4. Every Cloudy Has A Sliver Line.\n5. Every Cloudy Has A Sliver Line.\n6. Every Cloudy Has A Sliver Line.")
+                Rectangle()
                     .foregroundColor(.white)
-                    .fontWeight(.heavy)
-                    .font(.system(.headline, design: .rounded))
-                    .padding()
-                    .background(Color.gray)
-                    .cornerRadius(50)
-                    .opacity(0.8)
-                    .padding(),
-                alignment: .top
+                    .opacity(0.4)
             )
     }
 }
